905fe32b91c65155773ee3c9edaeb7469a575718
Oleksandr Shekera <oleksandrshekera@gmail.com>
2023-04-01 16:16:47 +0300

    Created start game function

--- TicTacToe.py
+++ TicTacToe.py
@@ -60,6 +60,40 @@ class TicTacToe:
     def swap_player_turn(self, player):
         return 'X' if player == 'O' else 'O'
 
+    def start(self):
+        self.create_board()
+
+        player = 'X' if self.get_random_first_player() == 1 else 'O'
+        while True:
+            print(f"Player {player} turn")
+
+            self.show_board()
+
+            # taking user input
+            row, col = list(
+                map(int, input("Enter row and column numbers to fix spot: ").split()))
+            print()
+
+            # fixing the spot
+            self.fix_spot(row - 1, col - 1, player)
+
+            # checking whether current player is won or not
+            if self.is_player_win(player):
+                print(f"Player {player} wins the game!")
+                break
+
+            # checking whether the game is draw or not
+            if self.is_board_filled():
+                print("Match Draw!")
+                break
+
+            # swapping the turn
+            player = self.swap_player_turn(player)
+
+        # showing the final view of board
+        print()
+        self.show_board()
+
 # starting the game
 tic_tac_toe = TicTacToe()
 tic_tac_toe.start()
