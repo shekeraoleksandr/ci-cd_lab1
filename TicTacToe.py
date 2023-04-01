d806dea2be591bff2cef4634a0364b9860452ea0
Oleksandr Shekera <oleksandrshekera@gmail.com>
2023-04-01 16:15:32 +0300

    Created function that checks whether the player has won

--- TicTacToe.py
+++ TicTacToe.py
@@ -8,6 +8,55 @@ class TicTacToe:
     def get_random_first_player(self):
         return random.randint(0, 1)
 
+    def is_player_win(self, player):
+        win = None
+
+        n = len(self.board)
+
+        # checking rows
+        for i in range(n):
+            win = True
+            for j in range(n):
+                if self.board[i][j] != player:
+                    win = False
+                    break
+            if win:
+                return win
+
+        # checking columns
+        for i in range(n):
+            win = True
+            for j in range(n):
+                if self.board[j][i] != player:
+                    win = False
+                    break
+            if win:
+                return win
+
+        # checking diagonals
+        win = True
+        for i in range(n):
+            if self.board[i][i] != player:
+                win = False
+                break
+        if win:
+            return win
+
+        win = True
+        for i in range(n):
+            if self.board[i][n - 1 - i] != player:
+                win = False
+                break
+        if win:
+            return win
+        return False
+
+        for row in self.board:
+            for item in row:
+                if item == '-':
+                    return False
+        return True
+
 
 # starting the game
 tic_tac_toe = TicTacToe()
