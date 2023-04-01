324df9c25361df6167aeb8072cf14c99df3458f0
Oleksandr Shekera <oleksandrshekera@gmail.com>
2023-04-01 16:57:42 +0300

    Bug fix

--- TicTacToe.py
+++ TicTacToe.py
@@ -2,11 +2,22 @@ import random
 
 
 class TicTacToe:
+
     def __init__(self):
         self.board = []
 
+    def create_board(self):
+        for i in range(3):
+            row = []
+            for j in range(3):
+                row.append('-')
+            self.board.append(row)
+
     def get_random_first_player(self):
         return random.randint(0, 1)
+
+    def fix_spot(self, row, col, player):
+        self.board[row][col] = player
 
     def is_player_win(self, player):
         win = None
@@ -57,8 +68,21 @@ class TicTacToe:
                     return False
         return True
 
+    def is_board_filled(self):
+        for row in self.board:
+            for item in row:
+                if item == '-':
+                    return False
+        return True
+
     def swap_player_turn(self, player):
         return 'X' if player == 'O' else 'O'
+
+    def show_board(self):
+        for row in self.board:
+            for item in row:
+                print(item, end=" ")
+            print()
 
     def start(self):
         self.create_board()
@@ -94,12 +118,7 @@ class TicTacToe:
         print()
         self.show_board()
 
-    def show_board(self):
-        for row in self.board:
-            for item in row:
-                print(item, end=" ")
-            print()
+
 # starting the game
 tic_tac_toe = TicTacToe()
 tic_tac_toe.start()
-
