93633224b708bc53b38cf05c6bff1410923c3dbd
Oleksandr Shekera <oleksandrshekera@gmail.com>
2023-04-02 13:27:51 +0300

    Created test function file for fis_board_filled

---
+++ tests/test_is_board_filled.py
@@ -0,0 +1,21 @@
+import pytest
+from TicTacToe import TicTacToe
+@pytest.fixture
+def game():
+    tic_tac_toe = TicTacToe()
+    tic_tac_toe.create_board()
+    return tic_tac_toe
+def test_is_board_filled(game):
+    game.board = [
+        ['X', 'O', 'X'],
+        ['X', 'O', 'X'],
+        ['O', 'X', 'O']
+    ]
+    assert game.is_board_filled() == True
+
+    game.board = [
+        ['X', '-', 'X'],
+        ['X', 'O', 'X'],
+        ['O', 'X', 'O']
+    ]
+    assert game.is_board_filled() == False
