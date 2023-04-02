49f70a944159da5029756c4ef0baaf7d1ae15f35
Oleksandr Shekera <oleksandrshekera@gmail.com>
2023-04-02 13:24:29 +0300

    Created test function file for create_board

---
+++ tests/test_create_board.py
@@ -0,0 +1,11 @@
+import pytest
+from TicTacToe import TicTacToe
+@pytest.fixture
+def game():
+    tic_tac_toe = TicTacToe()
+    tic_tac_toe.create_board()
+    return tic_tac_toe
+def test_create_board(game):
+    assert len(game.board) == 3
+    assert all(len(row) == 3 for row in game.board)
+    assert all(item == '-' for row in game.board for item in row)
