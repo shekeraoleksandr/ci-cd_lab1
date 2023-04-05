bf9491e4f954a981f2a971353454b22f26c2d67f
Oleksandr Shekera <oleksandrshekera@gmail.com>
2023-04-05 13:12:15 +0300

    Updated files correcting errors according flake8 recommendations

--- tests/test_is_board_filled.py
+++ tests/test_is_board_filled.py
@@ -15,11 +15,11 @@ def test_is_board_filled(game):
         ['X', 'O', 'X'],
         ['O', 'X', 'O']
     ]
-    assert game.is_board_filled() == True
+    assert game.is_board_filled()
 
     game.board = [
         ['X', '-', 'X'],
         ['X', 'O', 'X'],
         ['O', 'X', 'O']
     ]
-    assert game.is_board_filled() == False
+    assert not game.is_board_filled()
--- tests/test_is_player_win.py
+++ tests/test_is_player_win.py
@@ -15,25 +15,25 @@ def test_is_player_win(game):
         ['-', '-', '-'],
         ['-', '-', '-']
     ]
-    assert game.is_player_win('O') == True
+    assert game.is_player_win('O')
 
     game.board = [
         ['X', '-', '-'],
         ['X', '-', '-'],
         ['X', '-', '-']
     ]
-    assert game.is_player_win('X') == True
+    assert game.is_player_win('X')
 
     game.board = [
         ['X', '-', '-'],
         ['-', 'X', '-'],
         ['-', '-', 'X']
     ]
-    assert game.is_player_win('X') == True
+    assert game.is_player_win('X')
 
     game.board = [
         ['X', '-', 'O'],
         ['O', 'O', 'X'],
         ['X', 'O', '-']
     ]
-    assert game.is_player_win('O') == False
+    assert not game.is_player_win('O')
